2874c60bf07ed2b20f4796f18515a01295c230b1
Kevin Peset <kevin.peset@gmail.com>
2024-07-01 18:19:09 +0200

    Ajout Step 2 : Fetch API

---
+++ client/src/components/RecipeCard.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+export default function RecipeCard({ recipe }) {
+  console.info("Recipe depuis RecipeCard : ", recipe);
+  return (
+    <article>
+      <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+      <h2>{recipe.strMeal}</h2>
+      <p>{recipe.strArea}</p>
+      <p>{recipe.strCategory}</p>
+    </article>
+  );
+}
+
+RecipeCard.propTypes = {
+  recipe: PropTypes.shape({
+    strMealThumb: PropTypes.string.isRequired,
+    strMeal: PropTypes.string.isRequired,
+    strArea: PropTypes.string.isRequired,
+    strCategory: PropTypes.string.isRequired,
+  }).isRequired,
+};
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import axios from "axios";
+
+import RecipeCard from "../components/RecipeCard";
+
 export default function Home() {
+  const [recipe, setRandomeRecipe] = useState(null);
+
+  const getRandomRecipe = () => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/random.php")
+      .then((response) => setRandomeRecipe(response.data.meals[0]))
+      .catch((error) => console.error(error));
+  };
+
   return (
     <>
       <h1>Ratatouille</h1>
       <p>Bienvenue sur notre site qui contient des milliards de recettes !</p>
+
+      {/* Si je n'ai pas cliqué sur le bouton, je veux afficher "veuillez cliquer
+        sur le bouton" sinon, je veux afficher la recette provenant de l'api */}
+      <section className="random_recipe">
+        {!recipe ? (
+          <h3>Cliquez sur le bouton</h3>
+        ) : (
+          <RecipeCard recipe={recipe} />
+        )}
+      </section>
+      <button type="button" onClick={getRandomRecipe}>
+        Générer une recette
+      </button>
     </>
   );
 }
